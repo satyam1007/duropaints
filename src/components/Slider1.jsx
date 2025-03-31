794c15afdb77b76ff169340fcb0ffd6a24938ece
Apple <apple@Apples-MacBook-Pro.local>
2025-03-31 12:25:20 +0530

    changes

--- src/components/Slider1.jsx
+++ src/components/Slider1.jsx
@@ -37,6 +37,8 @@ function Slider1() {
   const settings = {
     dots: false,
     infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -50,7 +52,7 @@ function Slider1() {
         <div className="slide-inner bg-no-repeat bg-cover bg-center slide-bg-image bg-[url('https://www.dartdesign.in/images/portfolio/berger-paints.webp')] h-[400px] lg:h-[800px] col:h-[450px]">
           <div className="slide relative h-full bg-cover z-10 after:absolute after:content-[''] after:w-full after:h-full after:left-0 after:top-0 after:opacity-70 after:z-10">
             <div className="wraper max-w-[1200px] mx-auto">
-              <div className="slide-caption w-[700px] mt-[90px] lg:mt-[210px] lg:ml-[30px] ml-[10px] col:mt-[100px] absolute top-0 z-20">
+              <div className="slide-caption w-[700px] mt-[100px] lg:mt-[210px] lg:ml-[30px] ml-[10px] col:mt-[100px] absolute top-0 z-20">
                 <p className="bg-[#0d1b2a] opacity-75 p-2 rounded text-[#F5F5F5] lg:text-[20px] text-[12px] text-wrap lg:w-full md:w-full w-[320px] font-bold col:text-[18px]">
                   Transforming Spaces with Premium Paints & Expert Solutions
                 </p>
@@ -61,8 +63,8 @@ function Slider1() {
                 <div className="btns">
                   <div className="btn-style">
                     <a
-                      className="bg-[#41a4c3] relative cursor-pointer rounded text-[15px] font-semibold text-white px-[18px] py-[8px] lg:px-[38px] lg:py-[12px] lg:text-[16px] lg:inline-block mt-[6px] transition ease-in-out duration-300 hover:bg-[#31839d] col:mb-[5px] col:mt-[15px] before:absolute before:content-[''] before:w-[85px] before:h-[2px] before:left-[-60px] before:top-[50%] before:bg-white before:transform before:translate-y-[-50%] before:transition-all hover:before:left-[-95px] lg:before:block before:hidden"
-                      href="/contact"
+                      className="bg-[#41a4c3] relative cursor-pointer rounded text-[15px] font-semibold text-white px-[18px] py-[8px] lg:px-[38px] lg:py-[12px] lg:text-[16px] lg:inline-block mt-[6px] transition ease-in-out duration-300 hover:bg-[#31839d] active:bg-[#31839d] col:mb-[5px] col:mt-[15px] before:absolute before:content-[''] before:w-[85px] before:h-[2px] before:left-[-60px] before:top-[50%] before:bg-white before:transform before:translate-y-[-50%] before:transition-all hover:before:left-[-95px] lg:before:block before:hidden"
+                      href="#"
                     >
                       Get a Free Consultation
                     </a>
@@ -78,7 +80,7 @@ function Slider1() {
         <div className="slide-inner bg-no-repeat bg-cover bg-center slide-bg-image bg-[url('https://www.dartdesign.in/images/portfolio/bergerpaints7.webp')] h-[400px] lg:h-[800px] col:h-[450px]">
           <div className="slide relative h-full bg-cover z-10 after:absolute after:content-[''] after:w-full after:h-full after:left-0 after:top-0 after:opacity-70 after:z-10">
             <div className="wraper max-w-[1200px] mx-auto">
-              <div className="slide-caption w-[700px] mt-[90px] lg:mt-[210px] lg:ml-[30px] ml-[10px] col:mt-[100px] absolute top-0 z-20">
+              <div className="slide-caption w-[700px] mt-[100px] lg:mt-[210px] lg:ml-[30px] ml-[10px] col:mt-[100px] absolute top-0 z-20">
                 <p className="bg-[#0d1b2a] opacity-75 p-2 rounded text-[#F5F5F5] lg:text-[20px] text-[12px] text-wrap lg:w-full md:w-full w-[320px] font-bold col:text-[18px]">
                   Transforming Spaces with Premium Paints & Expert Solutions
                 </p>
@@ -89,8 +91,8 @@ function Slider1() {
                 <div className="btns">
                   <div className="btn-style">
                     <a
-                      className="bg-[#41a4c3] relative cursor-pointer rounded text-[15px] font-semibold text-white px-[18px] py-[8px] lg:px-[38px] lg:py-[12px] lg:text-[16px] lg:inline-block mt-[6px] transition ease-in-out duration-300 hover:bg-[#31839d] col:mb-[5px] col:mt-[15px] before:absolute before:content-[''] before:w-[85px] before:h-[2px] before:left-[-60px] before:top-[50%] before:bg-white before:transform before:translate-y-[-50%] before:transition-all hover:before:left-[-95px] lg:before:block before:hidden"
-                      href="/contact"
+                      className="bg-[#41a4c3] relative cursor-pointer rounded text-[15px] font-semibold text-white px-[18px] py-[8px] lg:px-[38px] lg:py-[12px] lg:text-[16px] lg:inline-block mt-[6px] transition ease-in-out duration-300 hover:bg-[#31839d] active:bg-[#31839d] col:mb-[5px] col:mt-[15px] before:absolute before:content-[''] before:w-[85px] before:h-[2px] before:left-[-60px] before:top-[50%] before:bg-white before:transform before:translate-y-[-50%] before:transition-all hover:before:left-[-95px] lg:before:block before:hidden"
+                      href="#"
                     >
                       Get a Free Consultation
                     </a>
--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -5,9 +5,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { GrNext } from "react-icons/gr";
 import { GrPrevious } from "react-icons/gr";
-import { FaFacebookF } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
 import { MdHealthAndSafety } from "react-icons/md";
 import { GiJerusalemCross } from "react-icons/gi";
 import { RiGitRepositoryPrivateFill } from "react-icons/ri";
@@ -70,10 +67,10 @@ const Slider2 = () => {
     <div className="max-w-[1200px] mx-auto mt-[100px]">
       <div className="relative">
         <div className="mb-[30px] text-center">
-          <span className="lg:text-[30px] text-[20px] font-bold font-[cursive] text-[#41a4c3]">
+          <span className="lg:text-3xl text-2xl font-bold font-[cursive] text-[#41a4c3]">
             What We Do
           </span>
-          <h2 className="lg:text-[40px] text-[30px] font-bold text-[#333] pb-[20px] relative before:absolute before:content-[''] before:left-[50%] before:bottom-0 before:transform before:-translate-x-1/2 before:w-[60px] before:h-[3px] before:bg-[#41a4c3]">
+          <h2 className="lg:text-4xl text-2xl font-bold text-[#333] pb-[20px] relative before:absolute before:content-[''] before:left-[50%] before:bottom-0 before:transform before:-translate-x-1/2 before:w-[60px] before:h-[3px] before:bg-[#41a4c3]">
             Our Services
           </h2>
         </div>
@@ -85,76 +82,62 @@ const Slider2 = () => {
             imgSrc: "/images/paint-benefits.jpg",
             icon: <MdHealthAndSafety />,
             name: "Contractors Benefits",
-            para: "",
           },
           {
             id: 2,
             imgSrc: "/images/paint-distribution.jpg",
             icon: <GiJerusalemCross />,
             name: "Paint Distribution/Wholesale",
-            para: "",
           },
           {
             id: 3,
             imgSrc: "/images/paint-private-jobs.avif",
             icon: <RiGitRepositoryPrivateFill />,
             name: "Private Label Jobs",
-            para: "",
           },
           {
             id: 4,
             imgSrc: "/images/paint-recycling.jpg",
             icon: <MdRecycling />,
             name: "Paint Recycling",
-            para: "",
           },
           {
             id: 5,
             imgSrc: "/images/paint-store.webp",
             icon: <FaStore />,
             name: "Store Franchisee",
-            para: "",
           },
           {
             id: 6,
             imgSrc: "/images/paint-supply.jpg",
             icon: <LuPaintBucket />,
             name: "Bulk Paint Supply",
-            para: "",
           },
-        ].map(({ id, imgSrc, icon, name, para }) => (
+        ].map(({ id, imgSrc, icon, name }) => (
           <div
             key={id}
             className="overflow-hidden relative p-0 group"
             style={{ width: "100%", display: "inline-block" }}
           >
             {/* Image Wrapper with Hover Effect */}
-            <div className="expert-img relative lg:w-80 h-80 lg:px-4 px-0 overflow-hidden rounded shadow-md">
+            <div className="expert-img relative lg:w-80 lg:h-80 h-96 lg:px-4 px-0 overflow-hidden rounded shadow-md">
               <img
                 alt={name}
                 src={imgSrc}
-                className="w-full h-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-110"
+                className="w-full h-full object-cover transition-transform duration-500 ease-in-out lg:group-hover:scale-110 group-hover:scale-0"
                 style={{ color: "transparent" }}
               />
             </div>
 
             {/* Info Box */}
-            <div className="relative text-center mt-[-40px] bg-[#f5f5f5] transform translate-y-[40px] transition ease-in-out duration-300 group-hover:translate-y-0 group-hover:bg-[#fff] py-4">
+            <div className="relative text-center mt-[-40px] bg-[#f5f5f5] transform lg:translate-y-[40px] translate-y-[-40px] transition ease-in-out duration-300 group-hover:translate-y-0 group-hover:bg-[#fff] py-4">
               <div className="flex justify-center items-center gap-2 flex-col">
                 <span className="text-[#41a4c3] lg:text-[50px] text-[35px]">
                   {icon}
                 </span>
                 <h3 className="text-[20px] col:text-[20px] font-medium font-heading-font">
-                  <a
-                    className="text-[#282e3f] hover:text-[#ada282] transition-all"
-                    href="/attorneys-single/Darcy-Alec"
-                  >
-                    {name}
-                  </a>
+                  <a className="text-[#282e3f] transition-all">{name}</a>
                 </h3>
-                <p className="text-gray-600 text-[16px] lg:text-[18px] text-center leading-relaxed">
-                  {para}
-                </p>
               </div>
             </div>
           </div>
