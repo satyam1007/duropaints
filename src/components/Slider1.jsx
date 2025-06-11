82b912ebd910da0cac8dd22a9eda1e52ef960912
Apple <apple@Apples-MacBook-Pro.local>
2025-06-11 18:45:46 +0530

    changes

--- src/components/Slider1.jsx
+++ src/components/Slider1.jsx
@@ -3,31 +3,32 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { GrNext, GrPrevious } from "react-icons/gr";
-import "./Slider1.css";
 
 const images = [
-  "https://dublinpaint.com/images/slides/1.jpg",
-  "https://dublinpaint.com/images/slides/4.jpg",
-  "https://dublinpaint.com/images/slides/6.jpg",
-  "https://dublinpaint.com/images/slides/3.jpg",
+  "/public/images/duerow_img_1.png",
+  "/public/images/duerow_img_2.png",
+  "/public/images/duerow_img_3.png",
+  "/public/images/duerow_img_4.png",
 ];
 
-// Arrows
+// Custom Arrows with better styling
 const PrevArrow = ({ onClick }) => (
   <button
-    className="absolute top-1/2 left-2 sm:left-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 sm:p-3 rounded-full z-10 hover:bg-opacity-80 transition cursor-pointer"
+    className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white bg-opacity-80 text-gray-800 p-2 rounded-full z-10 hover:bg-opacity-100 transition-all duration-300 cursor-pointer shadow-lg hover:scale-110"
     onClick={onClick}
+    aria-label="Previous slide"
   >
-    <GrPrevious size={20} className="sm:size-6" />
+    <GrPrevious size={24} />
   </button>
 );
 
 const NextArrow = ({ onClick }) => (
   <button
-    className="absolute top-1/2 right-2 sm:right-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 sm:p-3 rounded-full z-10 hover:bg-opacity-80 transition cursor-pointer"
+    className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white bg-opacity-80 text-gray-800 p-2 rounded-full z-10 hover:bg-opacity-100 transition-all duration-300 cursor-pointer shadow-lg hover:scale-110"
     onClick={onClick}
+    aria-label="Next slide"
   >
-    <GrNext size={20} className="sm:size-6" />
+    <GrNext size={24} />
   </button>
 );
 
@@ -43,15 +44,19 @@ const Slider1 = () => {
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
     fade: true,
+    dotsClass: "slick-dots slick-thumb",
+    customPaging: (i) => (
+      <div className="w-3 h-3 rounded-full bg-white bg-opacity-50 hover:bg-opacity-100 transition-all duration-300"></div>
+    ),
   };
 
   return (
-    <div className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-screen relative overflow-hidden">
+    <div className="w-full h-[82vh] max-h-[800px] relative overflow-hidden shadow-xl">
       <Slider {...settings}>
         {images.map((img, index) => (
-          <div key={index}>
+          <div key={index} className="relative">
             <div
-              className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-screen bg-cover bg-center"
+              className="w-full h-[82vh] max-h-[800px] bg-cover bg-center bg-no-repeat"
               style={{ backgroundImage: `url(${img})` }}
             />
           </div>
